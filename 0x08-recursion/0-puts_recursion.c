351ca4d33c38287764e02a08ea31e47666faf3fc
yoamzil <amzilyouness@gmail.com>
2023-04-05 12:42:08 -0700

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,11 +4,13 @@
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
  */
-void	_puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	int	i;
-
-	i = 0;
-	_putchar(s[i]);
-	_puts_recursion(*s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
